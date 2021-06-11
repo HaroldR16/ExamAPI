a325f4b2953bf7d83b217e997b8b86c821c160b0
HaroldR16 <josu.rea@outlook.com>
2021-06-10 20:39:34 -0600

    YOOOO

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/api/hello' , (req , res)=>{
+    res.json('hello from my simple server hehehehe')
+})
+
+app.post('/api/name' , (req , res)=>{
+    let name = req.body.name;
+    res.json(`Hello my name is ${name} Reaza`)
+
+})
+
+app.listen(port, ()=>{
+    console.log("Esta wea jala en el puerto: ", port)
+})
